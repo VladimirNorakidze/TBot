966e5ae52abdadd77a1868138be1f1ec16e4da3a
Владимир Норакидзе <vladimir@MacBook-Pro-Vladimir.local>
2019-05-15 02:09:07 +0300

    renaming

--- analyzer.py
+++ analyzer.py
@@ -4,7 +4,7 @@ import pymorphy2 as pm2
 # import module that will extract recommends
 
 
-def main(msg: str):
+def wa_analyzer(msg: str):
     morphy = pm2.MorphAnalyzer()
 
     norm_msg_pos = "_".join([morphy.normal_forms(x)[0] for x in re.findall(r"\w+", msg)])
@@ -12,5 +12,6 @@ def main(msg: str):
     return norm_msg_pos
 
 
-if __name__ == "__main__":
-    main(sys.argv[1])
+def img_analyzer(picture):
+    with open("test.jpg", "wb") as img:
+        img.write(picture.content)
