c4a7e770fe0deb502c1be1cf8c59d25198074282
Nati098 <32599882+Nati098@users.noreply.github.com>
2019-05-18 00:59:25 +0300

    Add beauty to code

--- botTextBrain.py
+++ botTextBrain.py
@@ -1,14 +1,12 @@
 import numpy as np
 import pandas as pd
 
-import re
-import pymorphy2
-from functools import lru_cache
-
 from annoy import AnnoyIndex
 from gensim.models import KeyedVectors, Word2Vec
 
-
+import re
+import pymorphy2
+from functools import lru_cache
 morph = pymorphy2.MorphAnalyzer()
 
 
@@ -27,14 +25,25 @@ class VisBotTextBrain:
         self.model = KeyedVectors.load(model_file)  # Word2Vec.load
         self.annoy = AnnoyIndex(self.model.wv.vector_size)
         self.annoy.load(annoy_file)
-
+        
     def run(self, request):
         """
         :param: request: str value
         :return: list of indexes in DB
         """
         request_list = normalize_text(request).split(' ')
-
+        
+        if (len(request_list)==1)and(request_list[0]==''):
+            raise ValueError('Incorrect request')
+        
+        vect_repr = self._get_vect_representation(request_list)
+    
+        self.request = request
+        self.request_vector = vect_repr
+    
+        return self.annoy.get_nns_by_vector(vect_repr, n=10)
+    
+    def _get_vect_representation(self, request_list):
         vect_repr = []
         for word in request_list:
             try:
@@ -46,15 +55,15 @@ class VisBotTextBrain:
             vect_repr = np.mean(np.array(vect_repr), axis=0)
         else:
             vect_repr = np.array([0]*300)
-    
-        self.request = request
-        self.request_vector = vect_repr
-    
-        return self.annoy.get_nns_by_vector(vect_repr, n=10)
+        
+        return vect_repr
 
 
 def main(request):
     model_file = "w2v.model"
     annoy_file = "annoy15"
     vbtb = VisBotTextBrain(model_file, annoy_file)
-    vbtb.run(request)
+    try:
+        vbtb.run(request)
+    except ValueError:
+        print('Incorrect request, bro. Type another string')
